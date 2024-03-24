7f2fdbaf3c481eacb0b4fa4e8e48033f179cf429
Can Cellek <can.cellek@gmail.com>
2024-03-24 14:25:27 +0300

    Fix lint issues

--- src/utils/command.js
+++ src/utils/command.js
@@ -41,7 +41,7 @@ function openFilteredLinks(command, settings) {
 		DefaultSearch(command, settings)
 	} else {
 		filteredUrls.map((url, index) => {
-			const target = index === filterCount - 1 ? settings.urlLaunch.target : "_blank" 
+			const target = index === filterCount - 1 ? settings.urlLaunch.target : "_blank"
 			openLink(url, target)
 		})
 	}
