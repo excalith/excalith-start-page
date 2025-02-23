550831fd61ccfd40e7db650c318cfa35b0793cb8
Can Cellek <can.cellek@gmail.com>
2025-02-23 12:05:45 +0300

    #65 Add custom title option

--- src/components/Meta.js
+++ src/components/Meta.js
@@ -11,7 +11,7 @@ const Meta = () => {
 
 	useEffect(() => {
 		// Set title
-		setTitle(settings.username + " Start Page")
+		setTitle(settings.title ? settings.title : settings.username + " Start Page")
 
 		// Return if there is no icon
 		if (!settings.fetch.image) return
